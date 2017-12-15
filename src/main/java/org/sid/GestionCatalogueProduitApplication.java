1d8cbff876d958949751ad23b87029034ac9cbe7
abdel <idouhammou.a@gmail.Com>
2017-12-15 12:25:39 +0100

    squelette du projet

---
+++ src/main/java/org/sid/GestionCatalogueProduitApplication.java
@@ -0,0 +1,12 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCatalogueProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCatalogueProduitApplication.class, args);
+	}
+}
