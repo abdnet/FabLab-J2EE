ffd1f3bd85ad5fcd88f955478b97ac318a170f91
abdel <idouhammou.a@gmail.Com>
2017-12-16 15:04:30 +0100

    Couche Web : Liste des produits + recherche d'un produit par designation

--- src/main/java/org/sid/GestionCatalogueProduitApplication.java
+++ src/main/java/org/sid/GestionCatalogueProduitApplication.java
@@ -1,12 +1,24 @@
 package org.sid;
 
+import org.sid.dao.ProduitRepository;
+import org.sid.entities.Produit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class GestionCatalogueProduitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionCatalogueProduitApplication.class, args);
+		ApplicationContext ctx=SpringApplication.run(GestionCatalogueProduitApplication.class, args);
+		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
+		produitRepository.save(new Produit("Prod 1",314,122));
+		produitRepository.save(new Produit("Prod 2",14,1));
+		produitRepository.save(new Produit("Prod 3",31,2));
+		produitRepository.save(new Produit("Prod 4",341,22));
+		produitRepository.save(new Produit("Prod 5",314,11));
+		produitRepository.save(new Produit("Prod 6",134,112));
+		produitRepository.findAll().forEach(s->System.out.println(s));
+
 	}
 }
---
+++ src/main/java/org/sid/web/ProduitController.java
@@ -0,0 +1,38 @@
+package org.sid.web;
+
+import java.util.List;
+import java.util.Set;
+
+import org.sid.dao.ProduitRepository;
+import org.sid.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import scala.annotation.meta.setter;
+
+@Controller
+public class ProduitController {
+	@Autowired
+	//injection de dependence
+	private ProduitRepository produitRepository;
+	
+	@RequestMapping(value="/index")
+	public String index(Model model,
+			@RequestParam(name="page",defaultValue="0")int p,
+			@RequestParam(name="size",defaultValue="5")int s,
+			@RequestParam(defaultValue="") String mc){
+		Page<Produit> produits= produitRepository.chercher("%"+mc+"%",new PageRequest(p, s));
+		model.addAttribute("ListProduit",produits.getContent());
+		int[] pages=new int[produits.getTotalPages()];
+		model.addAttribute("pages",pages);
+		model.addAttribute("currentp",p);
+		model.addAttribute("motCle",mc);
+		return "produits";
+	}
+
+}
