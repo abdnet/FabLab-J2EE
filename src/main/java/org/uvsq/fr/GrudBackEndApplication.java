a45ff47376df7bffe204ad496b9a534308541fa8
abdel <idouhammou.a@gmail.Com>
2018-01-22 18:24:33 +0100

    add projet

---
+++ src/main/java/org/uvsq/fr/GrudBackEndApplication.java
@@ -0,0 +1,12 @@
+package org.uvsq.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrudBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrudBackEndApplication.class, args);
+	}
+}
